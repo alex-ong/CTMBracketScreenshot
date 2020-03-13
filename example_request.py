1c4d9aa692818a5ed81d7494a629710ac891eb47
alex-ong <the.onga@gmail.com>
2020-03-14 03:53:08 +1100

    New and improved request with download

--- example_request.py
+++ example_request.py
@@ -2,8 +2,20 @@ import requests
 import json
 import time
 
+server_root = "http://127.0.0.1:8080/"
 with open('config.json') as f:
     data = json.load(f)
 t = time.time()
-r = requests.post('http://127.0.0.1:8080/api/', json=data)
+r = requests.post(server_root+'api/', json=data)
+
 print(r.json())
+print("The response took: " + str(time.time() - t) + "seconds")
+
+print("downloading files from server...")
+t = time.time()
+r = r.json()
+if 'files' in r:
+    for file in r['files']:
+        response = requests.get(server_root+file)
+        print(len(response.content))
+print("total download took" + str(time.time() - t) + "seconds")
